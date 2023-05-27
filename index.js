21c5454c98c127210d5b567f98d61bf4e0809a9a
Julian Johannesen <julian.johannesen@gmail.com>
2023-05-27 16:40:34 +0000

    Annotate the mutation observer script

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+// The buttons
 const addNodes = document.querySelector("#add-nodes");
 const removeNodes = document.querySelector("#remove-nodes");
 const reset = document.querySelector("#reset");
+// Node counter
 const counter = document.querySelector("#counter");
+// Node display area
 const target = document.querySelector("#target");
 let totalAddedNodes = 0;
 
+// 'Add Node' button listener
 addNodes.addEventListener("click", () => {
   const newPara = document.createElement("p");
   newPara.textContent = `Current time: ${Date.now()}`;
   target.appendChild(newPara);
 });
 
+// 'Remove Node' button listener
 removeNodes.addEventListener("click", () => {
   const lastChild = target.lastChild;
   if (lastChild) {
@@ -18,17 +23,20 @@ removeNodes.addEventListener("click", () => {
   }
 });
 
+// 'Reset' button listener
 reset.addEventListener("click", () => self.location.reload());
 
+// Log any newly added nodes
 function logNewNodes(records) {
-  for (const record of records) {
-    // Check if the childlist of the target node has been mutated
-    if (record.type === "childList") {
-      totalAddedNodes = totalAddedNodes + record.addedNodes.length;
-      // Log the number of nodes added
-      counter.textContent = `Total added nodes: ${totalAddedNodes}`;
+    for (const record of records) {
+        // Check if the childlist of the target node has been mutated
+        if (record.type === "childList") {
+            // Set the new total number of added nodes
+            totalAddedNodes = totalAddedNodes + record.addedNodes.length;
+            // Log the number of nodes added
+            counter.textContent = `Total added nodes: ${totalAddedNodes}`;
+        }
     }
-  }
 }
 
 const observer = new MutationObserver(logNewNodes);
